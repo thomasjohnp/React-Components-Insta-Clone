fcfe91427280f656c7ddda2de8b734952a0b8f56
thomasjohnp <58680041+thomasjohnp@users.noreply.github.com>
2020-03-17 18:03:51 -0400

    brain... needs sleep

--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -6,22 +6,17 @@ import React from "react";
 const LikeSection = props => {
   return (
     <div>
-    <div
-      className="like-section"
-      key="likes-icons-container"
-    >
-      <div className="like-section-wrapper">
-        <i className="far fa-heart" />
+      <div className="like-section" key="likes-icons-container">
+        <div className="like-section-wrapper">
+          <i onClick={props.addLike} className="far fa-heart" />
+        </div>
+        <div className="like-section-wrapper">
+          <i className="far fa-comment" />
+        </div>
       </div>
-      <div className="like-section-wrapper">
-        <i className="far fa-comment" />
-      </div>
+      <p className="like-number">{props.likeCount} likes</p>
     </div>
-    <p className="like-number">
-      
-      likes</p>
-</div>
-  )
+  );
 };
 
 export default LikeSection;
--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -1,22 +1,25 @@
 // You will add code in this file
-import React from "react";
+import React, {useState} from "react";
 import CommentSection from "../CommentSection/CommentSectionContainer";
 import LikeSection from "./LikeSection";
 import PostHeader from "./PostHeader";
-
+import dummyData from "../../dummy-data";
 import "./Posts.css";
 
 // pass props in this file to
 const Post = props => {
   // set up state for the likes
+  const [likeCount, setLikeCount] = useState(props.post.likes);
+
+  const updateLikes = () => {
+    setLikeCount(likeCount + 1);
+  };
 
   return (
     <div className="post-border">
       <PostHeader
         username={props.post.username}
-        thumbnailUrl={
-          props.post.thumbnailUrl
-        }
+        thumbnailUrl={props.post.thumbnailUrl}
       />
       <div className="post-image-wrapper">
         <img
@@ -25,7 +28,8 @@ const Post = props => {
           src={props.post.imageUrl}
         />
       </div>
-      <LikeSection />
+
+      <LikeSection likeCount={likeCount} addLike={updateLikes} />
       <CommentSection
         postId={props.post.imageUrl}
         comments={props.post.comments}
@@ -35,5 +39,3 @@ const Post = props => {
 };
 
 export default Post;
-
-
